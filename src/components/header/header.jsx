6de2c0f6d38810bf9b01c8b0005dfa34c9965ea0
shihongxia <1054689185@qq.com>
2020-05-15 15:06:07 +0800

    feat：header静态页面

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 import './header.less'
+import logo from '../../assets/images/logo.png'
 export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        Header
+        <div className="header-top">
+          <span>欢迎，admin</span>
+          <a href = "javascript:">退出</a>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">首页</div>
+          <div className="header-bottom-right">
+            <span>2019-2-12-2</span>
+            <img src={logo} alt="whether"></img>
+            <span>晴</span>
+          </div>
+        </div>
       </div>
     )
   }
